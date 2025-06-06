34e539291c3b578b78f673478715dddffacbc951
Thomas L <tito_tom@hotmail.fr>
2025-06-06 23:44:05 +0200

    feat: content script loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
@@ -8,4 +9,17 @@ export default defineConfig({
     vue(),
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        content: resolve(__dirname, 'src/content_scripts/content.ts'),
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          return chunkInfo.name === 'content' ? 'content.js' : 'assets/[name]-[hash].js'
+        },
+      },
+    },
+  },
 })
